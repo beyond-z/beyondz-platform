f08cb67e3b80107c982526c241a06eab9d035323
Brian Sadler <brian.sadler@beyondz.org>
2014-02-13 11:37:43 -0800

    Implement form submission writing to Google Sheet with stubs for user specific info.

--- app/controllers/submit_controller.rb
+++ app/controllers/submit_controller.rb
@@ -8,20 +8,43 @@ class SubmitController < ApplicationController
   # Action to POST the assignment submission to the Leadership Coach.
   def create 
           
-    #BTODO: move username/password to config files so they aren't checked into source ctrl
-
     # You can also use OAuth. See document of
     # GoogleDrive.login_with_oauth for details.
     session = GoogleDrive.login(ENV['GOOGLE_DRIVE_EMAIL'], ENV['GOOGLE_DRIVE_PASSWORD'])
 
-    ws = session.spreadsheet_by_title('TestAssignmentSubmit').worksheets[0]
-
-    # Changes content of cells.
-    # Changes are not sent to the server until you call ws.save().
-    ws[2, 1] = "foo"
-    ws[2, 2] = "bar"
+    # Initially set to key = 0AhkyYmQz77njdHpMeXRpNFUtZHViaWxQMWpfVkpuZmc which corresponds to the 
+    # "Assignment Submissions - Career Prep and Leadership Academy.gdoc"
+    doc = session.spreadsheet_by_key(ENV['GOOGLE_SUBMISSION_DOC_KEY'])
+    
+    ws = doc.worksheet_by_title(ws_name(params[:userEmail]))
+   
+    # Assuming the worksheet has the following columns, populate it.
+    # Student Name  | Student Email  |  Assignment  |   Submission URL 
+    firstEmptyRow = ws.num_rows() + 1
+    ws[firstEmptyRow, 1] = student_name(:userEmail)
+    ws[firstEmptyRow, 2] = params[:userEmail] 
+    ws[firstEmptyRow, 3] = assignment_name(params[:assignment_id])
+    ws[firstEmptyRow, 4] = params[:submissionUrl]
     ws.save()    
 
   end
+
+   # Takes an email address and looks up the name of the worksheet for that student
+   def ws_name(userEmail)
+    #BTODO:
+    return "School 1"
+  end
+
+   # Looks up the student's name from their email address
+   def student_name(userEmail)
+     #BTODO:
+     return "Joe Smoe"
+   end
+
+   # Translates the specified assignmentId to the display name
+   def assignment_name(assignmentId)
+     #BTODO:
+     return assignmentId
+   end
 
 end
