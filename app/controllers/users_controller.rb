66fded0fa89a97a0912f50434ce1e47da6346b8a
Adam D. Ruppe <destructionator@gmail.com>
2014-04-08 23:33:17 -0400

    indentation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,112 +1,112 @@
 require 'digest/sha1';
 
 class UsersController < ApplicationController
-    # ################
-    before_filter :prepare
-    def prepare
-        if session["user_id"] != nil
-          @user_logged_in = true
-          @user = User.find(session["user_id"].to_i)
-        else
-          @user_logged_in = false
-        end
+  # ################ FIXME: this should be done site-wide
+  before_filter :prepare
+  def prepare
+    if session["user_id"] != nil
+      @user_logged_in = true
+      @user = User.find(session["user_id"].to_i)
+    else
+      @user_logged_in = false
     end
-    # #################
+  end
+  # #################
 
-    def login_page
-        login_form(params[:email], nil)
-    end
+  def login_page
+    login_form(params[:email], nil)
+  end
 
-    def login_form(email, message)
-        @user = User.new
-        @email = email
-        @message = message
-        render action: 'form'
-    end
+  def login_form(email, message)
+    @user = User.new
+    @email = email
+    @message = message
+    render action: 'form'
+  end
 
-    def forgotPassword
-        # returns the form
-    end
+  def forgotPassword
+      # returns the form
+  end
 
-    def passwordEmailSent
-        # view only
-    end
+  def passwordEmailSent
+      # view only
+  end
 
-    def resetPassword
-        user = User.find(params[:id])
-        if user == nil
-            throw "no such user"
-        end
-
-        if user.reset_token != params[:token]
-            throw "bad reset token"
-        end
-
-        if user.reset_expiration < Time.now
-            throw "reset expired"
-        end
-
-        user.reset_token = ""
-        user.reset_expiration = Time.now
-        user.save;
-
-        # log in immediately with the reset token and send
-        # them to change their password now
-        session[:user_id] = user.id
-        redirect_to "/users/change-password"
+  def resetPassword
+    user = User.find(params[:id])
+    if user == nil
+      throw "no such user"
     end
 
-    def changePassword
-        # view
+    if user.reset_token != params[:token]
+      throw "bad reset token"
     end
 
-    def doChangePassword
-        user = User.find(session[:user_id])
-        if user == nil
-            throw "not logged in"
-        end
-
-        begin
-            user.changePassword(params[:password], params[:confirm_password]);
-            user.save;
-
-            redirect_to("/users/password-changed")
-        rescue
-            @message = "Your passwords didn't match, please try again."
-            render :changePassword
-        end
+    if user.reset_expiration < Time.now
+      throw "reset expired"
     end
 
-    def passwordChanged
-        #view
+    user.reset_token = ""
+    user.reset_expiration = Time.now
+    user.save;
+
+    # log in immediately with the reset token and send
+    # them to change their password now
+    session[:user_id] = user.id
+    redirect_to "/users/change-password"
+  end
+
+  def changePassword
+      # view
+  end
+
+  def doChangePassword
+    user = User.find(session[:user_id])
+    if user == nil
+      throw "not logged in"
     end
 
-    def doForgotPassword
-        email = params[:email]
+    begin
+      user.changePassword(params[:password], params[:confirm_password]);
+      user.save;
 
-        begin
-            User.forgotPassword(email, "http://#{request.host}/users/reset-password")
-            redirect_to "/users/password-email-sent"
-            rescue LoginException => e
-                @message = "That email address isn't on file. If you can't remember the email address you use to log in, please contact support."
-                render :forgotPassword
-        end
+      redirect_to("/users/password-changed")
+    rescue
+      @message = "Your passwords didn't match, please try again."
+      render :changePassword
     end
+  end
 
-    def logout
-        session[:user_id] = nil
-        @user_logged_in = false
-        render :logged_out
-    end
+  def passwordChanged
+    #view
+  end
 
-    def login
-        userInfo = params[:user]
-        begin
-            user = User.new
-            session[:user_id] = user.login(userInfo[:email], userInfo[:password])
-            redirect_to "/"
-            rescue LoginException => e
-                login_form(userInfo[:email], e.type)
-        end
+  def doForgotPassword
+    email = params[:email]
+
+    begin
+      User.forgotPassword(email, "http://#{request.host}/users/reset-password")
+      redirect_to "/users/password-email-sent"
+      rescue LoginException => e
+        @message = "That email address isn't on file. If you can't remember the email address you use to log in, please contact support."
+        render :forgotPassword
     end
+  end
+
+  def logout
+    session[:user_id] = nil
+    @user_logged_in = false
+    render :logged_out
+  end
+
+  def login
+    userInfo = params[:user]
+    begin
+      user = User.new
+      session[:user_id] = user.login(userInfo[:email], userInfo[:password])
+      redirect_to "/"
+      rescue LoginException => e
+        login_form(userInfo[:email], e.type)
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,59 +1,64 @@
 class LoginException < Exception
-	def initialize(type)
-		@type = type
-	end
+  def initialize(type)
+    @type = type
+  end
 
-	def type
-		@type
-	end
+  def type
+    @type
+  end
 end
 
 class User < ActiveRecord::Base
-    def new
+  def new
+  end
+
+  def create
+  end
+
+  # Returns the user ID of the matching user if the credentials
+  # pass, otherwise, raises a LoginException
+  def login(email, passw)
+    user = User.find_by email: email
+    if user == nil
+      raise LoginException.new("Incorrect email address")
+    else
+      parts = user.password.split("-")
+      salt = parts[0]
+      if parts[1] == User.hashPassword(salt, passw)
+        return user.id
+      else
+        raise LoginException.new("Incorrect password")
+      end
+    end
+  end
+
+  # Prepares a reset token for the account with the given
+  # email address and sends an email with the given link.
+  def self.forgotPassword(email, reset_link)
+    user = User.find_by email: email
+    if user == nil
+      raise LoginException.new("Incorrect email address")
+    else
+      user.reset_token = (0...8).map { (65 + rand(26)).chr }.join
+      user.reset_expiration = Time.now + 15.minutes
+      user.save();
+
+      reset_link += "?token=#{user.reset_token}&id=#{user.id}"
+      Notifications.forgot_password(email, user.name, reset_link).deliver
+    end
+  end
+
+  # Changes the user's password. Don't forget to call save afterward
+  def changePassword(newPassword, confirmPassword)
+    if(newPassword != confirmPassword)
+      raise "passwords don't match"
     end
 
-    def create
-    end
+    salt = (0...8).map { (65 + rand(26)).chr }.join
+    self.password = salt + "-" + User.hashPassword(salt, newPassword)
+  end
 
-    def login(email, passw)
-        user = User.find_by email: email
-        if user == nil
-            raise LoginException.new("Incorrect email address")
-        else
-            parts = user.password.split("-")
-            salt = parts[0]
-            if parts[1] == hashPassword(salt, passw)
-                return user.id
-            else
-                raise LoginException.new("Incorrect password")
-            end
-        end
-    end
-
-    def self.forgotPassword(email, reset_link)
-        user = User.find_by email: email
-        if user == nil
-            raise LoginException.new("Incorrect email address")
-        else
-            user.reset_token = (0...8).map { (65 + rand(26)).chr }.join
-            user.reset_expiration = Time.now + 15.minutes
-            user.save();
-
-            reset_link += "?token=#{user.reset_token}&id=#{user.id}"
-            Notifications.forgot_password(email, user.name, reset_link).deliver
-        end
-    end
-
-    def changePassword(newPassword, confirmPassword)
-        if(newPassword != confirmPassword)
-            raise "passwords don't match"
-        end
-
-        salt = (0...8).map { (65 + rand(26)).chr }.join
-        self.password = salt + "-" + hashPassword(salt, newPassword)
-    end
-
-    def hashPassword(salt, passw)
-            return Digest::SHA1.hexdigest("#{salt}#{passw}")
-    end
+  def self.hashPassword(salt, passw)
+    return Digest::SHA1.hexdigest("#{salt}#{passw}")
+  end
 end
