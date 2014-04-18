f1a0fce652708d4af5f383a82a6edf840ea456c7
Adam D. Ruppe <destructionator@gmail.com>
2014-04-17 21:12:09 -0400

    app/controllers/feedback_controller.rb

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -3,7 +3,7 @@ class FeedbackController < ApplicationController
     if request.post?
       f = Feedback
       f.feedback(params[:email], params[:message]).deliver
-      flash[:message] = "Thank you for your feedback!"
+      flash[:message] = "Thank you for your feedback! It's people like you that help make the Beyond Z experience great."
       redirect_to "/"
     end
   end
