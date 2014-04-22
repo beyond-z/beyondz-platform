55419d0c794ae0e04c88f3166fcdf4bf5ec77c26
Adam D. Ruppe <destructionator@gmail.com>
2014-04-22 11:39:39 -0400

    current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,20 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   # Setup user information for all controllers
-  before_filter :prepareUserInfo
-  def prepareUserInfo
+  before_filter :prepare_user_info
+  private
+  def prepare_user_info
     if session["user_id"] != nil
       @user_logged_in = true
       @user = User.find(session["user_id"].to_i)
     else
       @user_logged_in = false
+      @user = nil
     end
+  end
+
+  public
+  def current_user
+    return @user
   end
 end
