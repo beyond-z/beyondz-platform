86284df9b10921e8e254b0250fec6d3d935ac8b2
Brian Grayless <brian.grayless@sunergize.com>
2014-04-28 16:50:18 -0500

    Convert tabs to spaces.

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -8,71 +8,71 @@ class Assignment < ActiveRecord::Base
   scope :incomplete, -> { all.reject { |a| a.todos.incomplete.count == 0 } }
 
   state_machine :state, :initial => :new do
-  	# Define events and allowed transitions
-  	event :start do
-  		transition :new => :started
-  	end
+    # Define events and allowed transitions
+    event :start do
+      transition :new => :started
+    end
 
-  	event :submit do
-  		transition [:started, :pending_revision] => :pending_approval
-  	end
+    event :submit do
+      transition [:started, :pending_revision] => :pending_approval
+    end
 
-  	event :request_revision do
-  		transition :pending_approval => :pending_revision
-  	end
+    event :request_revision do
+      transition :pending_approval => :pending_revision
+    end
 
-  	event :approve do
-  		transition :pending_approval => :complete
-  	end
+    event :approve do
+      transition :pending_approval => :complete
+    end
 
-  	# Define state transition callbacks
-  	after_transition :on => :started, :do => :accept
-  	after_transition :on => :pending_approval, :do => :send_to_approver
-  	after_transition :on => :pending_revision, :do => :send_back_to_user
-  	before_transition :on => :complete, :do => :verify_assignment_completion
-  	after_transition :on => :complete, :do => :post_completion_check
+    # Define state transition callbacks
+    after_transition :on => :started, :do => :accept
+    after_transition :on => :pending_approval, :do => :send_to_approver
+    after_transition :on => :pending_revision, :do => :send_back_to_user
+    before_transition :on => :complete, :do => :verify_assignment_completion
+    after_transition :on => :complete, :do => :post_completion_check
 
-  	# Definte state attributes/methods
-  	state all - [:new, :completed] do
-  		def in_process?
-  			true
-  		end
-  	end
+    # Definte state attributes/methods
+    state all - [:new, :completed] do
+      def in_process?
+        true
+      end
+    end
 
-  	state :new, :completed do
-  		def in_process?
-  			false
-  		end
-  	end
+    state :new, :completed do
+      def in_process?
+        false
+      end
+    end
 
   end
 
 
   def initialize
-  	# NOTE: This *must* be called, otherwise states won't get initialized
-  	super()
+    # NOTE: This *must* be called, otherwise states won't get initialized
+    super()
   end
 
   def accept
-  	# user accepted/started assignment
-  	# notify coach?
+    # user accepted/started assignment
+    # notify coach?
   end
 
   def send_to_approver
-  	# request approval from coach
+    # request approval from coach
   end
 
   def send_back_to_user
-  	# request revision from user
+    # request revision from user
   end
 
   def verify_assignment_completion
-  	# verify all requirements are fulfilled before allowing completion
+    # verify all requirements are fulfilled before allowing completion
   end
 
   def post_completion_check
-  	# if all assignments are complete, notify coach?
-  	# lock assignments?
+    # if all assignments are complete, notify coach?
+    # lock assignments?
   end
 
 end
