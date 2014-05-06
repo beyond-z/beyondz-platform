280e50c587decf0abfa4243b9f4d4b12dcf72337
Adam D. Ruppe <destructionator@gmail.com>
2014-05-06 13:34:31 -0400

    starting documentation

--- app/models/task_definition.rb
+++ app/models/task_definition.rb
@@ -1,3 +1,6 @@
+# kind == 'user_confirm' means the user just checks it off (rendered as checkbox)
+# kind == 'file'         means upload file
+# kind == 'text'         means it expects text (rendered as textarea)
 class TaskDefinition < ActiveRecord::Base
 
   belongs_to :assignment_definition
