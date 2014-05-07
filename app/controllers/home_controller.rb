205959d8ef2570e1b66015960c31fdc70c57d9be
Adam D. Ruppe <destructionator@gmail.com>
2014-05-07 18:44:23 -0400

    redirect based on user role

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,14 @@
 class HomeController < ApplicationController
   def index
+    if current_user
+      if current_user.is_administrator?
+        redirect_to "/admin/"
+      elsif current_user.is_coach?
+        redirect_to "/coaches"
+      else
+        redirect_to "/assignments"
+      end
+    end
     @assignment_definitions = AssignmentDefinition.all
   end
 end
