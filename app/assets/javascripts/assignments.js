e0ecd7baa700d742c73c08d4a2944823c0dd9d5c
Brian Grayless <brian.grayless@sunergize.com>
2014-05-12 14:39:01 -0500

    Tweak changes after merge of another branch.

--- app/assets/javascripts/assignments.js
+++ app/assets/javascripts/assignments.js
@@ -1,22 +1,3 @@
 $(document).ready(function() {
 
-  $('.task-confirm').change(function() {
-    var assignment_id = $(this).data("assignment-id");
-    var task_id = $(this).data("task-id");
-    
-    $.ajax({
-      url: '/assignments/' + assignment_id + '/tasks/' + task_id + '.json',
-      type: 'PATCH',
-      data: {
-        'task': {
-          'user_confirm': this.checked
-        }
-      }
-    });
-    return false;
-  });
-
-  $('.assign-summary.completed').click(function() {
-    $(this).removeClass('completed');
-  })
 });
