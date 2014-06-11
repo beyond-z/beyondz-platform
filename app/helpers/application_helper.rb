aad64c30cabd382bca2ac45b17a7d1090d5a5b4c
Brian Grayless <brian.grayless@sunergize.com>
2014-06-11 12:54:59 -0500

    Fixed rubocop offenses.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,19 +10,20 @@ module ApplicationHelper
     end
   end
 
-
-  def modal(id, opts={})
-    opts = {}.merge(opts)
+  # generate standard-sized modal html
+  def modal(id, opts = {})
     modal_html(id, opts)
   end
 
-  def large_modal(id, opts={})
-    opts = {large: true}.merge(opts)
+  # generate large modal html
+  def large_modal(id, opts = {})
+    opts = { large: true }.merge(opts)
     modal_html(id, opts)
   end
 
-  def small_modal(id, opts={})
-    opts = {small: true}.merge(opts)
+  # generate small modal html
+  def small_modal(id, opts = {})
+    opts = { small: true }.merge(opts)
     modal_html(id, opts)
   end
 
@@ -31,49 +32,43 @@ module ApplicationHelper
 
   private
 
-    # define reusable and dynamic modal content
-    def modal_html(id, opts={})
-      options = {
+    def modal_options(opts = {})
+      {
         title: '',
         content: '',
-        footer: '<button type="button" class="btn btn-default"
-          data-dismiss="modal">Close</button>',
+        footer: '<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>',
         close_button: true,
         small: false,
         large: false
-      }
+      }.merge(opts)
+    end
 
-      options.merge!(opts)
-      
+    # define reusable and dynamic modal content
+    def modal_html(id, opts = {})
+
+      options = modal_options(opts)
+
       size = ''
       if options[:small]
         size = ' modal-sm'
       elsif options[:large]
-        size = 'modal-lg'
+        size = ' modal-lg'
       end
 
       html = ''
-      html += '<div class="modal fade" id="' + id + '" tabindex="-1"
-        role="dialog" aria-labelledby="' + id + '-label" aria-hidden="true">'
-        html += '<div class="modal-dialog' + size + '">'
-          html += '<div class="modal-content">'
-            html += '<div class="modal-header">'
-              if options[:close_button]
-                html += '<button type="button" class="close"
-                  data-dismiss="modal" aria-hidden="true">&times;</button>'
-              end
-              html += '<h4 class="modal-title" id="' + id + '-label">'
-                html += options[:title]
-              html += '</h4>'
-            html += '</div>'
-            html += '<div class="modal-body">'
-              html += options[:content]
-            html += '</div>'
-            html += '<div class="modal-footer">'
-              html += options[:footer]
-            html += '</div>'
-          html += '</div>'
-        html += '</div>'
+      html += '<div class="modal fade" id="' + id + '" tabindex="-1" role="dialog" aria-labelledby="' + id + '-label" aria-hidden="true">'
+      html += '<div class="modal-dialog' + size + '">'
+      html += '<div class="modal-content">'
+      html += '<div class="modal-header">'
+      if options[:close_button]
+        html += '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>'
+      end
+      html += '<h4 class="modal-title" id="' + id + '-label">' + options[:title] + '</h4>'
+      html += '</div>'
+      html += '<div class="modal-body">' + options[:content] + '</div>'
+      html += '<div class="modal-footer">' + options[:footer] + '</div>'
+      html += '</div>'
+      html += '</div>'
       html += '</div>'
 
       html.html_safe
