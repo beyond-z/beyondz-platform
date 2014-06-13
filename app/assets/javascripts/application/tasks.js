9e339f29e90bd4ee1b471ca72f3252d78b694bc7
Brian Grayless <brian.grayless@sunergize.com>
2014-06-13 12:02:32 -0500

    Cleaned up js/css a bit and made view logic more dynamic.

--- app/assets/javascripts/application/tasks.js
+++ app/assets/javascripts/application/tasks.js
@@ -1,21 +1,8 @@
 $(document).ready(function() {
   load_component_video_quiz();
- 
   load_component_comments();
+  load_component_compare_and_rank();
 
-  $(".document_comparison_rank").jRating({
-    rateMax: 5,
-    step: true,
-    bigStarsPath: '/images/jRating/stars.png',
-    smallStarsPath: '/images/jRating/small.png',
-    sendRequest: false,
-    canRateAgain: true,
-    showRateInfo: true,
-    nbRates: 5,
-    onClick: function(element, rate) {
-      //alert(rate);
-    }
-  });
 
   var task_action_box = $('.task-container .action-box');
   var task_update_submit = '#task-update-student .edit_task input[type=submit]';
