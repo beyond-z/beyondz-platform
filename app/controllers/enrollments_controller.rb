c3cc30a241c21cd36d9906de2a662ac05bf2dcdd
Brian Grayless <brian.grayless@sunergize.com>
2014-07-14 16:02:57 -0500

    Changed states to has to resolve rubocop issue.

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -3,7 +3,7 @@ class EnrollmentsController < ApplicationController
   layout 'public'
 
   def new
-    get_states
+    states
     @user = User.new
   end
 
@@ -35,7 +35,7 @@ class EnrollmentsController < ApplicationController
     @new_user = User.create(user)
 
     if @new_user.errors.any?
-      get_states
+      states
       @user = @new_user
       render 'new'
       return
@@ -55,17 +55,24 @@ class EnrollmentsController < ApplicationController
 
   private
 
-  def get_states
-    @states = [
-      'AL', 'AK', 'AZ', 'AR', 'CA', 'CO',
-      'CT', 'DE', 'DC', 'FL', 'GA', 'HI',
-      'ID', 'IL', 'IN', 'IA', 'KS', 'KY',
-      'LA', 'ME', 'MD', 'MA', 'MI', 'MN',
-      'MS', 'MO', 'MT', 'NE', 'NV', 'NH',
-      'NJ', 'NM', 'NY', 'NC', 'ND', 'OH',
-      'OK', 'OR', 'PA', 'RI', 'SC', 'SD',
-      'TN', 'TX', 'UT', 'VT', 'VA', 'WA',
-      'WV', 'WI', 'WY'
-    ]
+  def states
+    @states = {
+      "Alabama" => "AL", "Alaska" => "AK", "Arizona" => "AZ",
+      "Arkansas" => "AR", "California" => "CA", "Colorado" => "CO",
+      "Connecticut" => "CT", "Delaware" => "DE", "District of Columbia" => "DC",
+      "Florida" => "FL", "Georgia" => "GA", "Hawaii" => "HI", "Idaho" => "ID",
+      "Illinois" => "IL", "Indiana" => "IN", "Iowa" => "IA", "Kansas" => "KS",
+      "Kentucky" => "KY", "Louisiana" => "LA", "Maine" => "ME", "Maryland" => "MD",
+      "Massachusetts" => "MA", "Michigan" => "MI", "Minnesota" => "MN",
+      "Mississippi" => "MS", "Missouri" => "MO", "Montana" => "MT",
+      "Nebraska" => "NE", "Nevada" => "NV", "New Hampshire" => "NH",
+      "New Jersey" => "NJ", "New Mexico" => "NM", "New York" => "NY",
+      "North Carolina" => "NC", "North Dakota" => "ND", "Ohio" => "OH",
+      "Oklahoma" => "OK", "Oregon" => "OR", "Pennsylvania" => "PA",
+      "Rhode Island" => "RI", "South Carolina" => "SC", "South Dakota" => "SD",
+      "Tennessee" => "TN", "Texas" => "TX", "Utah" => "UT", "Vermont" => "VT",
+      "Virginia" => "VA", "Washington" => "WA", "West Virginia" => "WV",
+      "Wisconsin" => "WI", "Wyoming" => "WY"
+    }
   end
 end
