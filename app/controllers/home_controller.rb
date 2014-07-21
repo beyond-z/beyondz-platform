e3dde8cd87e8635e82d9b9df4d0222e4446ebab0
Adam D. Ruppe <destructionator@gmail.com>
2014-07-21 11:25:38 -0400

    I added comment to show how to do this.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
+  # The index will show the assignments until we make a dashboard.
   def index
     @assignment_definitions = AssignmentDefinition.all
   end
