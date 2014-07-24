42aec658e074a9a8d71b474e4fe29fbaa3752c21
Adam D. Ruppe <destructionator@gmail.com>
2014-07-24 16:10:25 -0400

    different email on staging

--- app/mailers/staff_notifications.rb
+++ app/mailers/staff_notifications.rb
@@ -3,6 +3,13 @@ class StaffNotifications < ActionMailer::Base
 
   def new_enrollment(new_user)
     @user = new_user
-    mail(to: 'signup-notification@beyondz.org', subject: 'New user sign up')
+
+    if Rails.env.staging?
+      to = 'tech@beyondz.org'
+    else
+      to = 'signup-notification@beyondz.org'
+    end
+
+    mail(to: to, subject: 'New user sign up')
   end
 end
