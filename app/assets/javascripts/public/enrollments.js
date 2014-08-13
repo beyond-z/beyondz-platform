650ad4b2cdee76ab7ebbe889da7668fcd2858649
Adam D. Ruppe <destructionator@gmail.com>
2014-08-13 12:32:07 -0400

    enrollments minimal

--- app/assets/javascripts/public/enrollments.js
+++ app/assets/javascripts/public/enrollments.js
@@ -1,4 +1,53 @@
 $(document).ready(function() {
+
+  jQuery(document).ready(function($) { 
+    // update the charater counter on textareas that have a maxlength property:
+    function updateCountdown(el) {
+      var maxlength = $(el).prop('maxlength')
+      var currentlength =  $(el).val().length;
+      $(el).next('.countdown').text(currentlength + '/' + maxlength);
+    }
+    // add a character counter textareas that have a maxlength property and make the count update as users type.
+    $('[maxlength]').change(function(){updateCountdown(this)}).keyup(function(){updateCountdown(this)}).after('<span class="countdown"></span>').change();
+
+    // set autofocus in case browser doesn't support html5
+      if (!('autofocus' in document.createElement('input'))) {
+        $(['autofocus']).focus();
+      }
+    
+    // expand fields based on radio button selection:
+    $('.expandable').children('.extra').hide();
+    $('.complex :radio').click(function(){
+      var parent = $(this).closest('.expandable');
+      $(parent).siblings('.expandable').children('.extra').slideUp('fast')
+      $(parent).children('.extra').slideDown('fast');
+    });
+    
+    // Show or hide questions based on user type and program:
+    $('[value=student]').click(function(){
+      console.log('student');
+      $('.coach').fadeOut('fast');
+      $('.student').fadeIn('fast');
+    });
+    $('[value=coach]').click(function(){
+      $('.student').fadeOut('fast');
+      $('.coach').fadeIn('fast');
+    });
+    
+    // Generate TOC:
+    $('h2').not('.sr-only, #form-almost-done h2').each(function() {
+      $('#jumplinks').append('<li class="page-jump"><a href="#'+$(this).closest('div').prop('id')+'"><div class="jump-icon"></div>'+$(this).text()+'</a></li>');
+    });
+
+    // Hide "other" checkbox/radio detail inputs 
+    //$('input.other').hide();
+    
+    // Fake send button:
+    $('#form-end').click(function(){
+      alert('This will lead to a confirmation screen. The next steps take place by email and phone, with admin-facing forms to sum up each step.');
+    });
+      
+  });
   
   // replace programming languages (testing only) with actual majors
   var majors = [
--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -1,11 +1,26 @@
 class EnrollmentsController < ApplicationController
 
+  before_filter :authenticate_user!
   before_action :use_controller_js
 
   layout 'public'
 
   def new
     @enrollment = Enrollment.new
+
+    # pre-fill fields that are available from the user
+    @enrollment.first_name = current_user.first_name
+    @enrollment.last_name = current_user.last_name
+    @enrollment.email = current_user.email
+  end
+
+  def show
+    # We'll show it by just displaying the pre-filled form
+    # as that's the fastest thing that can possibly work for MVP
+    @enrollment = Enrollment.find(params[:id])
+    render 'new'
+
+    # FIXME: permission check
   end
 
   def create
@@ -21,7 +36,8 @@ class EnrollmentsController < ApplicationController
   end
 
   def enrollment_params
-    params[:enrollment].permit(:user_id)
+    # allow all like 60 fields without listing them all
+    params.require(:enrollment).permit!
   end
 
 end
