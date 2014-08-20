50266b6f4f2620762334adaef7025235d199c2cd
Sarah Michaelson <smichaelson@amplify.com>
2014-08-19 20:54:51 -0400

    Add staff_email to secrets file for different environments. Use this secret to decide who to email for new user notification

--- app/mailers/staff_notifications.rb
+++ app/mailers/staff_notifications.rb
@@ -3,13 +3,6 @@ class StaffNotifications < ActionMailer::Base
 
   def new_user(new_user)
     @user = new_user
-
-    if Rails.env.production?
-      to = 'signup-notification@beyondz.org'
-    else
-      to = 'tech@beyondz.org'
-    end
-
-    mail(to: to, subject: 'New user sign up')
+    mail(to: Rails.application.secrets.staff_email, subject: 'New user sign up')
   end
 end
