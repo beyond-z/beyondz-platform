becec13287b60a5addfeb33c861ff3a912e8950c
Adam D. Ruppe <destructionator@gmail.com>
2014-08-26 12:22:03 -0400

    fix resume attachment

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -2,4 +2,15 @@ class Enrollment < ActiveRecord::Base
 
   belongs_to :user
 
+  has_attached_file :resume
+  validates_attachment :resume,
+                       content_type: {
+                         content_type: [
+                           'application/pdf',
+                           'application/msword',
+                           'application/vnd.openxmlformats-officedocument.wordpr
+ocessingml.document'
+                         ]
+                       },
+                       size: { in: 0..2.megabytes }
 end
