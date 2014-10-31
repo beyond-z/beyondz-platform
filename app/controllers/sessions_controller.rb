2e9c2e97b2bb75b56f061ac4085737858333cd3e
Adam D. Ruppe <destructionator@gmail.com>
2014-10-31 15:41:32 -0400

    experimental fix to sso double login for admin user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < Devise::SessionsController
   def new
-    unless params['plsin_login']
+    unless params['plain_login']
       redirect_to new_sso_user_session_path
     else
       super
