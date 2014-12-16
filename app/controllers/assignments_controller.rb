d50350a9bfef0a1ad090ffb6b0b44ca15b2f55a4
Adam D. Ruppe <destructionator@gmail.com>
2014-12-16 13:41:08 -0500

    splitting tasks into individual canvas pages

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -31,10 +31,20 @@ class AssignmentsController < ApplicationController
     # form friendlier links for use from the LMS, etc. too.
     unless params[:id] =~ /^\d+$/
       ad = AssignmentDefinition.find_by_seo_name(params[:id])
-      assignment = current_user.assignments.where(:assignment_definition_id => ad.id)
-      if assignment.any?
-        redirect_to assignment_path(assignment.first)
+      if params[:task]
+        # We want to look up a particular task by position so we
+        # can embed a specific item in an individual page
+        td = TaskDefinition.where(:assignment_definition_id => ad.id, :position => params[:task])
+        task = current_user.tasks.where(:task_definition_id => td.first.id)
+        redirect_to assignment_task_path(task.first.assignment_id, task.first)
         return
+      else
+        # If no particular task is requested, we'll send them to the front
+        assignment = current_user.assignments.where(:assignment_definition_id => ad.id)
+        if assignment.any?
+          redirect_to assignment_path(assignment.first)
+          return
+        end
       end
     end
 
