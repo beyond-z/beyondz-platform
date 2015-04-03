049562683e4399035917f8f8481349ead9e84852
Adam D. Ruppe <destructionator@gmail.com>
2015-04-03 14:01:14 -0400

    more options for college years

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,9 @@
 module UsersHelper
   def graduation_years
-    Time.now.year + 1 .. Time.now.year + 5
+    Time.now.year .. Time.now.year + 10
   end
 
   def started_college_years
-    Time.now.year - 5 .. Time.now.year
+    Time.now.year - 10 .. Time.now.year
   end
 end
