10ca3d8c6cf0a9eef5b4c7dbe07e853c4c63b2a4
Adam D. Ruppe <destructionator@gmail.com>
2015-04-03 14:21:04 -0400

    swap twitter around and check for html

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -213,10 +213,14 @@ class EnrollmentsController < ApplicationController
         cm.Digital_Footprint__c = @enrollment.online_resume
         cm.Digital_Footprint_2__c = @enrollment.online_resume2
       else
+        cm.Digital_Footprint__c = @enrollment.personal_website
         if @enrollment.twitter_handle && @enrollment.twitter_handle != ''
-          cm.Digital_Footprint__c = "https://twitter.com/#{@enrollment.twitter_handle}"
+          if @enrollment.twitter_handle.starts_with? 'http'
+            cm.Digital_Footprint_2__c = @enrollment.twitter_handle;
+          else
+            cm.Digital_Footprint_2__c = "https://twitter.com/#{@enrollment.twitter_handle}"
+          else
         end
-        cm.Digital_Footprint_2__c = @enrollment.personal_website
       end
 
       cm.Resume__c = @enrollment.resume.url if @enrollment.resume.present?
