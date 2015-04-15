50c8804213b5b3f35521c7a386b09f29fab29901
Brian Sadler <brian.sadler@beyondz.org>
2015-04-14 19:15:17 -0700

    Emails sent by the ActionMailer have improper Message-ID format and may be going to spam

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -1,4 +1,8 @@
+require 'digest/sha2'
 class DeviseMailer < Devise::Mailer
+  # needed because gmail was filtering some messages: http://blog.mailgun.com/tips-tricks-avoiding-gmail-spam-filtering-when-using-ruby-on-rails-action-mailer/
+  default "Message-ID" => ->(v){"<#{Digest::SHA2.hexdigest(Time.now.to_i.to_s)}@beyondz.org>"}
+
   def confirmation_instructions(record, token, opts = {})
     mail = super
     # your custom logic
