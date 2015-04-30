0626806ebf31a70d7aeac185443260680c2f58f4
Adam D. Ruppe <destructionator@gmail.com>
2015-04-29 23:20:07 -0400

    nice fixes

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,4 +1,23 @@
 class ConfirmationsController < Devise::ConfirmationsController
+  # See also: passwords_controller.rb for the same function, but
+  # this one needs to be able to take a custom message too and
+  # preserve the original behavior in some cases.
+  def set_flash_message(_key, kind, options = {}, message = nil)
+    message = find_message(kind, options) if message.nil?
+    cookies['login_flash'] = {
+      :value => message,
+      # We want this to be seen on the sso.beyondz.org (or stagingsso.beyondz.org)
+      # but can't set the cookie directly there - best we can do is to set to
+      # beyondz.org (note: staging.beyondz.org will NOT work as that won't
+      # include stagingsso.beyondz.org - where it needs to be. So no need to
+      # configure it - there is only one correct setting possible.
+      :domain => '.beyondz.org'
+    }
+
+    super(_key, kind, options) unless _key.nil?
+  end
+
+
   def new
     super
     if params[:email]
@@ -14,7 +33,7 @@ class ConfirmationsController < Devise::ConfirmationsController
     if !user.nil? && user.confirmed?
       path = after_resending_confirmation_instructions_path_for(resource_name)
       path += "?email=#{URI.encode_www_form_component(params[:user][:email])}"
-      flash[:notice] = 'You are already confirmed. Please try signing in'
+      set_flash_message(nil, nil, {}, 'You are already confirmed. Please try signing in')
       respond_with({}, :location => path)
     else
       # Otherwise, keep the behavior the same
