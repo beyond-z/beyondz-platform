5faa2f8d39b7fd7401a9bf94ecebddf0f13205f5
Adam D. Ruppe <destructionator@gmail.com>
2015-04-30 11:00:09 -0400

    add missing files

---
+++ app/controllers/passwords_controller.rb
@@ -0,0 +1,14 @@
+class PasswordsController < Devise::PasswordsController
+  def set_flash_message(_key, kind, options = {})
+    message = find_message(kind, options)
+    cookies['login_flash'] = {
+      :value => message,
+      # We want this to be seen on the sso.beyondz.org (or stagingsso.beyondz.org)
+      # but can't set the cookie directly there - best we can do is to set to
+      # beyondz.org (note: staging.beyondz.org will NOT work as that won't
+      # include stagingsso.beyondz.org - where it needs to be. So no need to
+      # configure it - there is only one correct setting possible.
+      :domain => '.beyondz.org'
+    }
+  end
+end
