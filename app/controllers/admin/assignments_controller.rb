07f090bec551b3f1bf381045f36ca7df09a1fa6f
Adam D. Ruppe <destructionator@gmail.com>
2016-02-10 12:04:32 -0500

    keep the email on validation fail

--- app/controllers/admin/assignments_controller.rb
+++ app/controllers/admin/assignments_controller.rb
@@ -69,17 +69,18 @@ class Admin::AssignmentsController < Admin::ApplicationController
   end
 
   def set_due_dates
+    @user_email = params[:email]
     # view render
   end
 
   def do_set_due_dates
     if params[:import].nil?
       flash[:message] = 'Please upload a csv file'
-      redirect_to admin_set_due_dates_path
+      redirect_to admin_set_due_dates_path(email: email)
       return
     end
 
-    email = params[:email]
+    email = params[:import][:email]
     file = CSV.parse(params[:import][:csv].read)
 
     # pre-load the assignments based on the first row
@@ -197,29 +198,29 @@ class Admin::AssignmentsController < Admin::ApplicationController
     rescue BadDateException => e
       flash[:error] = "#{e.message} is in the wrong format."
       flash[:message] = 'Please write dates and times in format YYYY-MM-DD HH:MM ZZZ. For example, "2015-12-25 12:00 EST" means noon in Eastern time on Christmas 2015.'
-      redirect_to admin_set_due_dates_path
+      redirect_to admin_set_due_dates_path(email: email)
       return
     rescue BadAssignmentException => e
       flash[:error] = "#{e.message}"
       flash[:message] = 'Go to the assignment you want on Canvas. At the end of the URL there will be a number like /assignments/21. The assignment ID there would be 21.'
-      redirect_to admin_set_due_dates_path
+      redirect_to admin_set_due_dates_path(email: email)
       return
     rescue DuplicateSectionNameException => e
       flash[:error] = "#{e.message}"
       flash[:message] = 'Make sure each cohort only appears once per assignment. You might want to get a fresh export of the spreadsheet to sync with Canvas and then make your edits on that.'
-      redirect_to admin_set_due_dates_path
+      redirect_to admin_set_due_dates_path(email: email)
       return
     rescue BadSectionNameException => e
       flash[:error] = "#{e.message}"
       flash[:message] = 'Make sure the cohort name in the row is an exact match. Also double-check the assignment ID and course ID columns. If this is supposed to be a new override, also make sure you remembered to clear out the override ID column too (and if it is supposed to edit an existing one, be sure that value is left the same from the export). Any given section should only appear once per assignment.'
-      redirect_to admin_set_due_dates_path
+      redirect_to admin_set_due_dates_path(email: email)
       return
     end
 
     self.delay.commit_new_due_dates(email, changed)
 
     flash[:message] = 'Changes in progress, you should check your email to know when it is complete.'
-    redirect_to admin_set_due_dates_path
+    redirect_to admin_set_due_dates_path(email: email)
   end
 
   def commit_new_due_dates(email, changed)
