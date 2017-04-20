5f69de2d26b8a1561bf9b308a4cde630cf5f67d9
Adam D. Ruppe <destructionator@gmail.com>
2017-04-20 14:21:37 -0400

    the validator in rails is too strict, not taking no for an answer (it can put nil in there evidently). just remove and depend on the client side thing to make them answer

--- app/models/champion.rb
+++ app/models/champion.rb
@@ -2,8 +2,6 @@ class Champion < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
-  validates :braven_fellow, presence: true
-  validates :braven_lc, presence: true
   validates :industries, presence: true
   validates :studies, presence: true
 
