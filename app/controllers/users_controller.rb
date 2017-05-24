5c301cc302df34db2850cef2afa4a0d392e3fdd1
Adam D. Ruppe <destructionator@gmail.com>
2017-05-24 10:28:56 -0400

    rubocop again

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,8 +27,7 @@ class UsersController < ApplicationController
   end
 
   def sso_discovery
-    self.class.layout "minimal"
-
+    self.class.layout 'minimal'
   end
 
   # We need to exempt this from csrf checking so the CAS server can
