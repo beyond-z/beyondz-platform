e27100e55f425f5fe4a67563375979899e7183dd
Adam D. Ruppe <destructionator@gmail.com>
2017-10-04 11:31:59 -0400

    stupid me

--- app/models/champion_contact.rb
+++ app/models/champion_contact.rb
@@ -7,8 +7,8 @@ class ChampionContact < ActiveRecord::Base
     # if a week has passed and the fellow hasn't answered the survey yet, we email them asking them to fill it out
     # similarly, if a week has passed after the contact request and the champion hasn't answered, we ask them too
     ChampionContact.where("
-      (fellow_survey_answered_at IS NULL OR champion_survey_answered_at IS NULL)
-      (fellow_survey_email_sent != TRUE OR champion_survey_email_sent != TRUE)
+      ((fellow_survey_answered_at IS NULL OR champion_survey_answered_at IS NULL)
+      AND (fellow_survey_email_sent != TRUE OR champion_survey_email_sent != TRUE))
       AND created_at < ?",
       1.week.ago.end_of_day).each do |cc|
       unless cc.fellow_survey_answered_at && !cc.fellow_survey_email_sent
