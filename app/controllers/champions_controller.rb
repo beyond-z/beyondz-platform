45c0332abf03a48604dc0ff368823956b9b53656
Jaime Bellmyer <online@bellmyer.com>
2018-04-24 17:43:50 -0500

    add mailchimp user creation to champion creation

--- app/controllers/champions_controller.rb
+++ app/controllers/champions_controller.rb
@@ -391,6 +391,7 @@ class ChampionsController < ApplicationController
     n.save
 
     n.create_on_salesforce
+    n.create_mailchimp
 
     if was_new
       ChampionsMailer.new_champion(n).deliver
