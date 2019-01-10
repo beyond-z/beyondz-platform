977505a997fc3b600749c8b454c9135facfb2928
Adam D. Ruppe <destructionator@gmail.com>
2019-01-10 16:59:03 -0500

    consistent variable name

--- app/controllers/mentor_controller.rb
+++ app/controllers/mentor_controller.rb
@@ -60,7 +60,7 @@ class MentorController < ApplicationController
     @number_of_weeks = campaign.PM_Number_Weeks__c.to_i
     @kickoff_location = campaign.PM_Kickoff_Location__c
     @due_date = format_date(campaign.PM_Application_Due_Date__c)
-    @start_date = format_date(campaign.PM_Kickoff_Date__c)
+    @kickoff_date = format_date(campaign.PM_Kickoff_Date__c)
     @end_date = format_date(campaign.PM_End_Date__c)
     @desired_industries = campaign.PM_Industries_List__c
 
@@ -74,7 +74,6 @@ class MentorController < ApplicationController
     #@kickoff_location = @program_site # FIXME
     #@due_date = "December 25, 2018" # FIXME
     #@kickoff_date = "January 1, 2019" # FIXME
-    #@start_date = "January 1, 2019" # FIXME
     #@end_date = "February 1, 2019" # FIXME
     #@desired_industries = "LIST INDUSTRIES" #FIXME
 
