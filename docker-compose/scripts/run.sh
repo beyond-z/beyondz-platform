05a78b800f0c1e38cddf58a25f673a420b5e4c69
Kasun Geethaka <geethakacdsdt@gmail.com>
2019-06-13 06:54:37 +0530

    Update run.sh

--- docker-compose/scripts/run.sh
+++ docker-compose/scripts/run.sh
@@ -11,9 +11,6 @@ cp -a /app/docker-compose/.env-docker /app/.env
 
 envsubst < /app/docker-compose/config/database.yml > /app/config/database.yml
 
-# check if database is provisioned or not
-if [[ $(psql -lqt -h joindb -p 5432 -U postgres postgres | cut -d \| -f 1 | grep -i beyondz | wc -l) = 0 ]]; then
-  bundle exec rake db:create; bundle exec rake db:migrate; bundle exec rake db:seed
-fi
+bundle exec rake db:create; bundle exec rake db:migrate; bundle exec rake db:seed
 
 bundle exec bin/rails s -p 3001 -b '0.0.0.0'
