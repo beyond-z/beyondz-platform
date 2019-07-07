76b8a6d1de36ec74ac6736104af45c71ee5ab78b
geethaka <kasun.geethaka@icloud.com>
2019-07-07 16:22:47 +0530

    add k8s run script

---
+++ docker-compose/scripts/run_kube.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# When you stop the container, it doesn't clean itself up properly so it fails to start next time. Cleanup!
+if [ -e /app/tmp/pids/server.pid ]; then
+  echo "Cleaning up previous server state"
+  rm /app/tmp/pids/server.pid
+fi
+
+cp -a /app/docker-compose/config/* /app/config/
+cp -a /app/docker-compose/.env-docker /app/.env
+
+envsubst < /app/docker-compose/config/database.yml > /app/config/database.yml
+
+#bundle exec rake db:create; bundle exec rake db:migrate; bundle exec rake db:seed
+
+bundle exec bin/rails s -p 3001 -b '0.0.0.0'
