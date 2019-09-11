64578c2f8a8881b984dd5e631ac9aa5b96448ccf
Brian Sadler <brian.sadler@beyondz.org>
2019-09-11 15:41:21 -0400

    Cleanup the docker dev env.
    Turn on debug logging. Don't try to reset the database with an empty
    seeds file on each restart. Don't remove the volumes on a rebuild. Devs
    should be responsible for explicitly deleting and recreating volumes if
    they need to nuke stuff. We shouldn't nuke by default.

--- docker-compose/scripts/docker_compose_run.sh
+++ docker-compose/scripts/docker_compose_run.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#sleep 15s until db is up and running
-sleep 15
-
 # When you stop the container, it doesn't clean itself up properly so it fails to start next time. Cleanup!
 if [ -e /app/tmp/pids/server.pid ]; then
   echo "Cleaning up previous server state"
@@ -13,6 +10,16 @@ fi
 # .env takes precedence over the env_file directive in docker-compose.yml
 cp -a /app/docker-compose/.env-docker /app/.env
 
-bundle exec rake db:create; bundle exec rake db:migrate; bundle exec rake db:seed
+# Here is a command to create an empty database with just the stuff in db:seed. We don't do this in
+# here b/c it would be run everytime docker starts when we only need to create/populate the database
+# once or on demand.
+#bundle exec rake db:create; bundle exec rake db:migrate; bundle exec rake db:seed
+echo "###"
+echo "If this is the first time building this and you havent created/populated your database yet, run these commands:"
+echo ""
+echo "docker-compose exec joinweb bundle exec rake db:create"
+echo "./docker-compose/scripts/dbrefresh.sh"
+echo ""
 
+echo "Starting rails app. Go to http://joinweb:3001 (assuming joinweb is in /etc/hosts on the host machine)"
 bundle exec bin/rails s -p 3001 -b '0.0.0.0'
