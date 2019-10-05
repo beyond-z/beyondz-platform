2021f87bae2f62bbad187153d2ebb3279b3f8b6f
Brian Sadler <sadler.brian@gmail.com>
2019-10-05 06:46:02 -0400

    Pass the Salesforce related config values into the dev env.
    I'm working on setting up a Salesforce Sandbox that we can use
    in the dev env. The first step is to have those values passed from
    your shell into the docker environment since we can't check them in.

--- docker-compose/scripts/docker_compose_run.sh
+++ docker-compose/scripts/docker_compose_run.sh
@@ -6,6 +6,29 @@ if [ -e /app/tmp/pids/server.pid ]; then
   rm /app/tmp/pids/server.pid
 fi
 
+echo "Checking if the SALESFORCE ENV vars are setup"
+if [ -z "$DATABASEDOTCOM_CLIENT_ID" ] || 
+   [ -z "$DATABASEDOTCOM_CLIENT_SECRET" ] || 
+   [ -z "$SALESFORCE_USERNAME" ] || 
+   [ -z "$SALESFORCE_PASSWORD" ] || 
+   [ -z "$SALESFORCE_SECURITY_TOKEN" ] || 
+   [ -z "$SALESFORCE_MAGIC_TOKEN" ] ; then
+  echo "The SALESFORCE related ENV vars arent setup. One of the following is empty: "
+  echo ""
+  echo "  DATABASEDOTCOM_CLIENT_ID=$DATABASEDOTCOM_CLIENT_ID"
+  echo "  DATABASEDOTCOM_CLIENT_SECRET=$DATABASEDOTCOM_CLIENT_SECRET"
+  echo "  SALESFORCE_USERNAME=$SALESFORCE_USERNAME"
+  echo "  SALESFORCE_PASSWORD=$SALESFORCE_PASSWORD"
+  echo "  SALESFORCE_SECURITY_TOKEN=$SALESFORCE_SECURITY_TOKEN"
+  echo "  SALESFORCE_MAGIC_TOKEN=$SALESFORCE_MAGIC_TOKEN"
+  echo ""
+  echo "Please make sure and set these in your shell (e.g. ~/.bash_profile) before starting the container!"
+  echo "The values to use are in: https://drive.google.com/a/bebraven.org/file/d/1AiwrCKZ11-BjNxIIP6qfD9B9rQvmfMY2/view?usp=sharing"
+  exit 1
+else
+  echo "Ok!"
+fi
+
 # Here is a command to create an empty database with just the stuff in db:seed. We don't do this in
 # here b/c it would be run everytime docker starts when we only need to create/populate the database
 # once or on demand.
