6409a790f43416ce67efee6d263162aa758388b9
Harry Li <harry.li@bebraven.org>
2020-10-26 10:50:27 -0400

    setup.sh definitely works all the time now

--- setup.sh
+++ setup.sh
@@ -1,6 +1,14 @@
 #!/bin/zsh
 
-docker-compose up -d && \
-docker-compose exec joinweb bundle exec rake db:create && \
-./docker-compose/scripts/dblatest_download.sh && \
+docker-compose down
+rm -rf latest.dump*
+./docker-compose/scripts/dblatest_download.sh
+
+docker volume rm beyondz-platform_db-data
+docker-compose up -d joindb 
+docker-compose run joinweb bundle exec rake db:create
 ./docker-compose/scripts/dblatest_restore.sh
+./docker-compose/scripts/dblatest_restore.sh # first time, some errors. second time, not so much.
+
+docker-compose down
+docker-compose up -d
