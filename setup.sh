e5b455098b8863389e41f74bf93f6fa9c12f3b59
Harry Li <harry.li@bebraven.org>
2020-10-26 17:15:14 -0400

    nap time

--- setup.sh
+++ setup.sh
@@ -5,7 +5,8 @@ rm -rf latest.dump*
 ./docker-compose/scripts/dblatest_download.sh
 
 docker volume rm beyondz-platform_db-data
-docker-compose up -d joindb 
+docker-compose up -d joindb
+sleep 5
 docker-compose run joinweb bundle exec rake db:create
 ./docker-compose/scripts/dblatest_restore.sh
 ./docker-compose/scripts/dblatest_restore.sh # first time, some errors. second time, not so much.
