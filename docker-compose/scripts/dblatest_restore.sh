ae00d8548d58bef8aa72b4bc34b40aa203a777d6
Harry Li <harry.li@bebraven.org>
2021-02-08 12:35:55 -0500

    fixing dbrefresh

--- docker-compose/scripts/dblatest_restore.sh
+++ docker-compose/scripts/dblatest_restore.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 echo "Refreshing your local dev database from the recent download"
 
-docker-compose exec joindb pg_restore --verbose --clean --jobs 6 --no-acl --no-owner -U postgres -d joindb /latest.dump
+docker-compose down
+docker-compose up -d joindb
+
+echo "DROP SCHEMA public CASCADE;" | docker-compose exec -T joindb psql -U postgres -d joindb
+echo "CREATE SCHEMA public;" | docker-compose exec -T joindb psql -U postgres -d joindb
+
+docker-compose exec joindb pg_restore --jobs 1 --no-acl --no-owner -U postgres -d joindb /latest.dump
 
 # This is our normal test password for everyone. I got this by loading a staging db with the security keys 
 # and stuff we use, then setting someones password to the test one in the rails console.
